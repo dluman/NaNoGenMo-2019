5d2f6e21095b6ca370bb828fbfa6aa441a4da23e
Douglas Luman <dluman@douglasjluman.com>
2019-12-07 22:49:32 +0000

    Removing ridiculous loop

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import Files
 
 import random
 
-for _ in range(12000):
-	try:
-		Grammar.main()
-	except:
-		print "Exception, but that's OK"
+try:
+	Grammar.main()
+except:
+	print "Exception, but that's OK"
